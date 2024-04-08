f9b6f631e85e322e576c67cc5539ee34527c58af
felix mavila <felixmacarenco@gmail.com>
2024-04-08 16:09:51 +0100

    first commit

---
+++ aula1.js
@@ -0,0 +1,14 @@
+function calcular(){
+    console.log('calculo')
+}
+
+
+const calcular2 = function(){
+    console.log('calculo2')
+}
+
+const calcular3 = ()=> console.log('calcular')
+
+calcular()
+calcular2()
+calcular3()
